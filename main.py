2b1e0b51f23b2a5f076412cb8ac1f4c740b8bd09
RodyCoco <enjoyrodycoco@gmail.com>
2021-11-03 14:54:27 +0800

    use VIT and cross validation

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
-import matplotlib.pyplot as plt
 import torch
-from torchvision import datasets
 import torchvision.transforms as tfs
 import numpy as np
-from PIL import Image 
-import torchvision.models as models
-from IPython.display import display
-from data_gen import BirdDataSet, load_label, GPU_NUMBER
-import torch.utils.data as Data
 from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
-import datetime
-import copy
-from model import Densenet161
-# learning rate
-# validation set
-# pretrain
+from model import Vit_large_patch16_224
+from PIL import Image 
+from data_gen import BirdDataSet, load_label, GPU_NUMBER
 
-lr = 10e-5
-epochs = 100
+lr = 5e-6
+epochs = 25
 batch_size = 32
+number_of_data_per_class = 15
 
 train_trans = tfs.Compose([
     tfs.Resize((224, 224), Image.BILINEAR),
     tfs.RandomHorizontalFlip(),
-    # tfs.ColorJitter(0.2,0.2,0.2,0.2),
     tfs.RandomRotation(degrees=45),
     tfs.ToTensor(),
     tfs.Normalize(mean=[0.5,0.5,0.5], std=[0.2, 0.2, 0.2])
@@ -36,39 +25,44 @@ valid_trans = tfs.Compose([
     tfs.Normalize(mean=[0.5,0.5,0.5], std=[0.2, 0.2, 0.2])
 ])
 
-def procedure():
+#indicate images that chosen in validation set
+valid_index = [[0,2], [3,5], [6,8], [9,11], [12,14]]
+
+def procedure(id):
+
+    print(f"round: {id+1}")
+
     torch.manual_seed(0)
     torch.cuda.manual_seed(0)
     torch.cuda.manual_seed_all(0)
     np.random.seed(0)
     
-    model = Densenet161().cuda(GPU_NUMBER)
-    model = torch.nn.DataParallel(model, device_ids=[0, 1, 2, 3])
+    model_name = "Vit_large_patch16_224"
+    model =  Vit_large_patch16_224().cuda(GPU_NUMBER)
+    model = torch.nn.DataParallel(model, device_ids=[0,1,2,3])
     model.double()
-    print("model done")
+    print(f"load {model_name} done")
 
-    number_of_data_per_class = 15
     train_data = []
     valid_data = []
     label = load_label()
     for i in range(len(label)):
-        if i%number_of_data_per_class>=9 and i%number_of_data_per_class<=11:
+        if i%number_of_data_per_class >=  valid_index[id][0] and i%number_of_data_per_class <= valid_index[id][1]:
             valid_data.append(label[i])
         else:
             train_data.append(label[i])
 
     train_data = BirdDataSet(main_dir="2021VRDL_HW1_datasets/training_images"\
-        ,data = train_data, transform = train_trans)
+                            ,data=train_data, transform=train_trans)
     train_loader = DataLoader(dataset=train_data, batch_size=batch_size, num_workers=4, shuffle=True)
     valid_data = BirdDataSet(main_dir="2021VRDL_HW1_datasets/training_images"\
-        ,data = valid_data, transform = valid_trans)
+                            ,data=valid_data, transform=valid_trans)
     valid_loader = DataLoader(dataset=valid_data, batch_size=batch_size, num_workers=4, shuffle=True)
     
-    optimizer = torch.optim.Adam(model.parameters(),lr=lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     loss_function = torch.nn.CrossEntropyLoss()
     decay = lambda epoch: 0.99 ** (epoch)
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=decay)
-    max_acc = -1
     min_loss = np.inf
 
     for epoch in range(epochs) :
@@ -78,14 +72,10 @@ def procedure():
         print("Epoch[{:3d}/{}]  train_loss:{} valid_acc:{} valid_loss:{}"\
             .format(epoch+1,epochs,train_loss,valid_acc,valid_loss))
         scheduler.step()
-        if valid_acc > max_acc:
-            torch.save(model.state_dict(), 'densenet161_acc_2.pkl')
-            max_acc = valid_acc
-            print("Save acc_model")
         if valid_loss < min_loss:
-            torch.save(model.state_dict(), 'densenet161_loss_2.pkl')
+            torch.save(model.state_dict(), f"{model_name}_{id}.pkl")
             min_loss = valid_loss
-            print("Save loss_model")
+            print(f"Save {model_name}_{id}.pkl")
 
 def train(model, train_loader, loss_function, optimizer):
     
@@ -125,4 +115,6 @@ def eval_loss(model,loader, loss_function):
     return sum(loss_list) / len(loss_list)
 
 if __name__ == '__main__':
-    procedure()
+    # use 5-fold cross-validation
+    for i in range(5):
+        procedure(i)
--- model.py
+++ model.py
@@ -1,14 +1,13 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.models as models
+import timm
 
-class Densenet161(nn.Module):
+class Vit_large_patch16_224(nn.Module):
     def __init__(self):
-        super(Densenet161, self).__init__()
-        self.densenet = models.densenet161(pretrained=True)
+        super(Vit_large_patch16_224, self).__init__()
+        self.model = timm.create_model('vit_large_patch16_224', pretrained=True)
         self.fc1 = nn.Linear(1000, 200)
     def forward(self, x):
-        x = self.densenet(x)
+        x = self.model(x)
         x = self.fc1(x)
         return x
+
