158cd177c65f82a763f5d6167c13d5c14e4e226d
RodyCoco <55498247+RodyCoco@users.noreply.github.com>
2021-11-04 13:30:00 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,13 +1,15 @@
 import torch.nn as nn
 import timm
 
+
 class Vit_large_patch16_224(nn.Module):
     def __init__(self):
         super(Vit_large_patch16_224, self).__init__()
-        self.model = timm.create_model('vit_large_patch16_224', pretrained=True)
+        self.model = timm.create_model(
+            'vit_large_patch16_224', pretrained=True)
         self.fc1 = nn.Linear(1000, 200)
+
     def forward(self, x):
         x = self.model(x)
         x = self.fc1(x)
         return x
-
